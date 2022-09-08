025f1117097ac6e2b19ce5f23ef8ad27b4a0ccde
Jane Fadhili <janefadhili@gmail.com>
2022-09-08 06:26:23 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
-/*
- * main- prints exactly "Programming is like building a multilingual puzzle
- * 
- * */
+/*function main- prints exactly "Programming is like building a multilingual puzzle*/
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
