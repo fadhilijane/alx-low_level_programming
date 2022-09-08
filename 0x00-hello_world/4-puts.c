ca148f88951497319465f42fd9e87b1261d5142d
Jane Fadhili <janefadhili@gmail.com>
2022-09-08 12:14:21 -0700

    correct

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *main- prints exactly "Programming is like building a multilingual puzzle
+ *main- prints exactly "Programming is like building a multilingual puzzle"
  *return 0 if exited properly, non-zero otherwise
- */
+ **/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
