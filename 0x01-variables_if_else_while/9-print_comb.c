5869f205faa8678b052b87cb4fe2f850e45c6722
Jane Fadhili <janefadhili@gmail.com>
2022-09-13 09:27:01 -0700

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * printed in ascending order
+ * separated by ,, followed by a space
+ * Return: 0 if true
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar('');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
