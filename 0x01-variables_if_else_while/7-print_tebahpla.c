62ca364640de478d0b59e4f9fe99d5d9f7e2941b
Jane Fadhili <janefadhili@gmail.com>
2022-09-13 09:08:32 -0700

    print

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse
+ * followed by a new line
+ * Return: 0 if uccessful
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
