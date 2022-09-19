5a65f6f64846319a6e0d4930278ddacad5abdfcd
Jane Fadhili <janefadhili@gmail.com>
2022-09-19 12:33:10 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ * @s: the argument
+ * Return: o if successful
+ */
+
+void print_rev(char *s)
+{
+	char rev = s[0];
+	int counter = 0;
+	int i;
+
+	while (s[counter] != 0)
+	{
+		counter++;
+	}
+	for (i = 0; i < fcounter; i++)
+	{
+		fcounter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
+	}
+}
