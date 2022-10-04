efbc55fa6742c07fae660d105bdf840ae9253c0a
Jane Fadhili <janefadhili@gmail.com>
2022-10-04 10:25:57 -0700

    point

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-#include <stdio.h>
+
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
@@ -9,6 +9,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
-
-#endif
+void set
