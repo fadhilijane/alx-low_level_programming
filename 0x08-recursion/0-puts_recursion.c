a5f1ff06f5064ac345a863669be94068626148c8
Jane Fadhili <janefadhili@gmail.com>
2022-10-05 06:08:02 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints's a string followed by a new line
+ * @s: string to be printed
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	/**
+	 * if the pointer index reaches the null terminator,
+	 *  newline & return
+	 */
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	/* otherwise, print the current array index */
+	_putchar(*s);
+	/* call function */
+	_puts_recursion(s + 1);
+}
