4d9c0381e604a6cf112d9a1c41b811c71a2a56ba
Jane Fadhili <janefadhili@gmail.com>
2022-10-05 07:37:39 -0700

    recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,53 +1,26 @@
 #include "main.h"
 
 /**
- * strlen_recursion - This function finds the length of a string
- * @s: The string to evaluate
- * Return: The length of the string s
+ * wildcmp - compares two strings and returns 1 if the strings
+ * can be considered identical, otherwise return 0.
+ * @s1: string to compare to
+ * @s2: string with wild character
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 
-int strlen_recursion(char *s)
+int wildcmp(char *s1, char *s2)
 {
-	/* if string is empty, return 1 as palindrome */
-	if (*s == '\0')
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
 	{
-		return (0);
+		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+			return (0);
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+			return (1);
 	}
-	s++;
-	return (1 + strlen_recursion(s));
-}
-
-/**
- * helper - This function evaluates a string
- * @s: The string to evaluate
- * @len: The length of the string
- * Return: 1 if s is a palindrom, otherwise 0
- */
-
-int helper(char *s, int len)
-{
-	/* if string is empty, return 1 as palindrome */
-	if (len <= 1)
-		return (1);
-	/* if current index is equal to half the index */
-	else if (*s == *(s + len - 1))
-		return (helper(s + 1, len - 2));
-	else
-		return (0);
-}
-
-/**
- * is_palindrome - This function evaluates whether a string is a palindrome
- * @s: The string to evaluate
- * Return: 1 if s is a palindrome, otherwise 0
- */
-
-int is_palindrome(char *s)
-{
-	int len;
-
-	len = strlen_recursion(s);
-	if (len <= 1)
-		return (1);
-	return (helper(s, len));
+	return (0);
 }
