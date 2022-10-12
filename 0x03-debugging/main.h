8b1552ef21d096914e664b333d79f666ee8441ee
Jane Fadhili <janefadhili@gmail.com>
2022-10-12 02:12:18 -0700

    deug

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,19 +1,6 @@
-ifndef MAIN_H
-#define MAIN_H
-
-
-
-#include <stdio.h>
-
-
-int _putchar(char c);
 void positive_or_negative(int i);
 
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 
 
-
-#endif
-Footer
-© 2022 GitHub
