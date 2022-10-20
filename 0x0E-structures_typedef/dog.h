aef5f2e04c37bef8d6be52e13690230adb0aefb6
Jane Fadhili <janefadhili@gmail.com>
2022-10-20 13:18:01 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _ DOG_H_
 
 
 /**
@@ -21,4 +21,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif
+#endif /*_DOG_H */
