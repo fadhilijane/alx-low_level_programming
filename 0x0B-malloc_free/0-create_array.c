4df243e63835169b74dca9192e87ba1bad20d488
Jane Fadhili Njaga <111033901+fadhilijane@users.noreply.github.com>
2022-10-24 18:01:39 +0300

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,14 +7,14 @@
 */
 char *create_array(unsigned int size, char c)
 {
-unsigned int i;
-char *s;
-if (size == 0)
-return (NULL);
-s = (char *)malloc(size * sizeof(char));
-if (s == NULL)
-return (NULL);
-for (i = 0; i < size; i++)
-s[i] = c;
-return (s);
+  unsigned int i;
+  char *s;
+  if (size == 0)
+    return (NULL);
+  s = (char *)malloc(size * sizeof(char));
+  if (s == NULL)
+    return (NULL);
+  for (i = 0; i < size; i++)
+    s[i] = c;
+  return (s);
 }
