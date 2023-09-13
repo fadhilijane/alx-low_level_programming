71ae127ac26cd1e1504d6c530f17a2d6694bf13e
fadhilijane <janefadhili@gmail.com>
2023-09-13 05:30:41 -0700

    linear searc

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,6 +12,10 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
+	if (array == NULL || size == 0)
+	{
+		return (-1);
+	}
 	for (i = 0; i < size; i++)
 	{
 		printf("value checked array[%lu] = [%d]\n", i, array[i]);
@@ -20,9 +24,6 @@ int linear_search(int *array, size_t size, int value)
 			return (i);
 		}
 	}
-	if (array == NULL || i == size)
-	{
-		return (-1);
-	}
+
 	return (-1);
 }
