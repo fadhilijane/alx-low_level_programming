1b3aec3667cf5e8a3be16d9362e926d251b620b4
Jane Fadhili Njaga <111033901+fadhilijane@users.noreply.github.com>
2023-09-14 04:58:27 +0300

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,5 +7,5 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
 /* helper functions */
-void print_array(int *array, size_t inc, size_t size);
+int recursive_search(int *array, size_t size, int value);
 #endif
