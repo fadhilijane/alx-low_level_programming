6ab8169a7c73b1e432cc5656d2a82ad49471f80c
Jane Fadhili Njaga <111033901+fadhilijane@users.noreply.github.com>
2023-09-14 04:52:45 +0300

    1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,37 +1,63 @@
 #include "search_algos.h"
 
 /**
- * binary_search - algo used for searching
- * @array:  pointer to the first element of the array to search in
- * @size:  number of elements in array
- * @value: value to search
- * Return: index
+ * binary_search - binary search a ascending sorted array to find a value.
+ * No duplicates in array. Must print subarray each split.
+ * @array: pointer to first element in array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: index of matched value in array or -1 if error or not found
  */
-
 int binary_search(int *array, size_t size, int value)
 {
-	size_t left = 0;
-	size_t right = size - 1;
-	size_t mid = size / 2;
-	size_t i;
+	int tmp;
+	size_t b = 0;
 
 	if (array == NULL || size == 0)
 		return (-1);
-
-	while (left <= right)
+	size--;
+	while (b <= size)
 	{
-		printf("Searching in array: ");
-		for (i = 0; i < size; i++)
-                {
-                        printf("%d", array[i]);
-                        printf(", ");
-                }
-                printf("\n");
+		print_array(array, b, size);
 
-		mid = (left + right) / 2;
-		if (value == array[mid])
-			return (mid);
-		else if (value < array[mid])
+		tmp = (size - b) / 2 + b;
+		if (array[tmp] == value)
+			return (tmp);
+		else if (array[tmp] < value)
+		{
+			if (size % 2 == 1)
+				b = tmp + 1;
+			else
+				b = tmp;
+		}
+		else
+		{
+			if (size % 2 == 1)
+				size = tmp;
+			else
+				size = tmp - 1;
+		}
+	}
+	return (-1);
+}
+
+/**
+ * print_array - print the current array given rules on format
+ * @array: array to print
+ * @inc: starting incrementer index
+ * @size: increment up to this size
+ */
+void print_array(int *array, size_t inc, size_t size)
+{
+	printf("Searching in array: ");
+	for (; inc <= size; inc++)
+	{
+		printf("%d", array[inc]);
+		if (inc < size)
+			printf(", ");
+	}
+	printf("\n");
+}
 		{
 			right = mid - 1;
 			size = right - left;
